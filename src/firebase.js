ac09e576e324b6e14670a6f06be85ba96fa9bbfa
Aleksanterijaakola <aleksanterijaakola@icloud.com>
2020-11-11 20:37:56 +0200

    stuff

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCb63pn8xPiMf_rkuL3gzOEfUtkEkgS2G4",
+  authDomain: "clone-4e931.firebaseapp.com",
+  databaseURL: "https://clone-4e931.firebaseio.com",
+  projectId: "clone-4e931",
+  storageBucket: "clone-4e931.appspot.com",
+  messagingSenderId: "426142062413",
+  appId: "1:426142062413:web:c46d8aa88a543b76024904",
+  measurementId: "G-BFRJJ7VP6B",
+};
