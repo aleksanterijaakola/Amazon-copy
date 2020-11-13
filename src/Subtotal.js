0cfb2289055d0be4438dde6302c5d33b6118f11e
Aleksanterijaakola <aleksanterijaakola@icloud.com>
2020-11-13 14:16:49 +0200

    Added more functionality with reducer

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
-
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from './reducer'
 
 function Subtotal() {
-
+  const [{ basket }, dispatch] = useStateValue();
 
   return (
     <div className="subtotal">
@@ -12,7 +13,6 @@ function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              {/* Part of the homework */}
               Subtotal ({basket.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
@@ -21,7 +21,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={getBasketTotal(basket)} // Part of the homework
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
